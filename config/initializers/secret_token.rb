19d8db1a8aa82e46416ecdee00c9f38586d91f04
JulianArnold <julian_arnold@hotmail.com>
2015-02-02 22:00:38 +0000

    first commit in a while

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Depot::Application.config.secret_token = 'e399e6201f7e5e5d7e89d460a6b1d65eb46c558e7d690f5dcb1f7742f6cfdfcba73214337828ff5ace90c2d9bbe354510d7ce6d04aadb336b6cb2349daec9981'
+Depot::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+                                           ('x' * 30) # meets minimum requirement of 30 chars long
+                                         else
+                                           ENV['SECRET_TOKEN']
+                                         end
--- db/migrate/20140616221516_combine_items_in_cart.rb
+++ db/migrate/20140616221516_combine_items_in_cart.rb
@@ -1,9 +1,11 @@
 class CombineItemsInCart < ActiveRecord::Migration
+
   def up
     # replace multiple items for a single product in a cart with a single item
     Cart.all.each do |cart|
-      # count the number of product(s) in the cart
+      # count the number of each product in the cart
       sums = cart.line_items.group(:product_id).sum(:quantity)
+
       sums.each do |product_id, quantity|
         if quantity > 1
           # remove individual items
@@ -19,12 +21,14 @@ class CombineItemsInCart < ActiveRecord::Migration
   end
 
   def down
-    # split items with quantity > 1 into multiple items
+    # split items with quantity>1 into multiple items
     LineItem.where("quantity>1").each do |line_item|
       # add individual items
       line_item.quantity.times do
-        LineItem.create cart_id: line_item.cart_id, product_id: line_item.product_id
+        LineItem.create cart_id: line_item.cart_id,
+                        product_id: line_item.product_id
       end
+
       # remove original item
       line_item.destroy
     end
